b44aa59b38c1ce1079d6a113bfc83a9b337102b1
Ecorralesz <estebcz@hotmail.com>
2025-03-11 06:53:17 -0700

    added hash route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import Results from './Pages/Results';
 import 'bootstrap/dist/css/bootstrap.min.css';
